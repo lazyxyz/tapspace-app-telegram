1c8c566c8ac033c0567e2855dc173d044bfaa128
duongtuancntt <duongtuan12a11@gmail.com>
2024-07-18 15:30:58 +0700

    update refetch

--- src/components/Footer/Menu/Resource/index.tsx
+++ src/components/Footer/Menu/Resource/index.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@chakra-ui/react";
+import { Stack, Text } from "@chakra-ui/react";
 import FrequencyResource from "./FrequencyResource";
 import { BitcoinProvider } from "@/components/Wrapper/BitcoinProvider";
 
@@ -6,6 +6,17 @@ export default function ResourcesDrawer({ isOpen, onClose }: any) {
   return (
     <Stack w={"full"}>
       <FrequencyResource />
+
+      <Text
+        fontSize={"sm"}
+        fontWeight={"medium"}
+        onClick={async () => {
+          localStorage.clear();
+          window.location.reload();
+        }}
+      >
+        Refresh
+      </Text>
     </Stack>
   );
 }
