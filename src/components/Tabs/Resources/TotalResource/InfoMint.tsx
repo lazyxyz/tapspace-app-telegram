03f848f10d7fd1cbf68c0b6a7b1a68054edda900
duongtuancntt <duongtuan12a11@gmail.com>
2024-07-30 15:19:28 +0700

    Update leaderboard

--- src/components/Tabs/Resources/TotalResource/InfoMint.tsx
+++ src/components/Tabs/Resources/TotalResource/InfoMint.tsx
@@ -1,9 +1,17 @@
 import { useTelegram } from "@/lib/TelegramProvider";
 import systemService from "@/services/system.service";
-import { HStack, Stack, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Image,
+  Stack,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { MemoizedMintItem } from "./MintItem";
+import Leaderboard from "@/components/Leaderboard";
 
 export type MintItemType = {
   capacity: number;
@@ -53,6 +61,7 @@ const InfoMint = ({ data, refetch }: any) => {
   const [totalItems, setTotalItems] = useState<number>(0);
 
   const totalItemsRef = useRef(totalItems);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     if (data && typeof window !== "undefined") {
@@ -229,51 +238,69 @@ const InfoMint = ({ data, refetch }: any) => {
   };
 
   return (
-    <VStack w="full" h="full" px={2} justifyContent="flex-start" pt={2}>
-      <HStack w={"full"}>
-        {listData.map((item) => (
-          <MemoizedMintItem
-            key={item.resource_name}
-            item={item}
-            accumulatedValues={accumulatedValues}
-          />
-        ))}
-      </HStack>
-      <Stack
-        py={3}
-        flex="1"
-        justifyContent="center"
-        alignItems="center"
-        w="full"
-        pb={24}
-      >
-        <motion.div
-          onClick={handleImageClick}
-          initial="normal"
-          variants={clickVariants}
-          whileTap="click"
-          style={{
-            display: "inline-block",
-            position: "relative",
-          }}
+    <>
+      <VStack w="full" h="full" px={2} justifyContent="flex-start" pt={2}>
+        <HStack w={"full"}>
+          {listData.map((item) => (
+            <MemoizedMintItem
+              key={item.resource_name}
+              item={item}
+              accumulatedValues={accumulatedValues}
+            />
+          ))}
+        </HStack>
+        <Stack
+          py={3}
+          flex="1"
+          justifyContent="center"
+          alignItems="center"
+          w="full"
+          pb={24}
         >
-          <motion.img
-            src="/assets/centerClick.png"
-            alt="Center Click"
-            animate={{ rotate: 360 }}
-            transition={{
-              repeat: Infinity,
-              duration: 90,
-              ease: "linear",
-            }}
+          <motion.div
+            onClick={handleImageClick}
+            initial="normal"
+            variants={clickVariants}
+            whileTap="click"
             style={{
               display: "inline-block",
-              filter: "drop-shadow(0px 4px 50px rgba(239, 103, 244, 0.45))",
+              position: "relative",
             }}
-          />
-        </motion.div>
-      </Stack>
-    </VStack>
+          >
+            <motion.img
+              src="/assets/centerClick.png"
+              alt="Center Click"
+              animate={{ rotate: 360 }}
+              transition={{
+                repeat: Infinity,
+                duration: 90,
+                ease: "linear",
+              }}
+              style={{
+                display: "inline-block",
+                filter: "drop-shadow(0px 4px 50px rgba(239, 103, 244, 0.45))",
+              }}
+            />
+          </motion.div>
+
+          <Box
+            bg={"rgba(255, 255, 255, 0.15)"}
+            borderWidth={1}
+            position={"absolute"}
+            bottom={"11%"}
+            right={"3%"}
+            borderBottomWidth={2}
+            rounded={"xl"}
+            borderColor={"rgba(255, 255, 255, 0.15)"}
+            onClick={onOpen}
+            p={1}
+          >
+            <Image src="/assets/leaderboard.png" w={"40px"} h={"40px"} />
+          </Box>
+        </Stack>
+      </VStack>
+      <Leaderboard isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+    </>
   );
 };
 
